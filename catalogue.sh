90355a59befc9f3589f2c182dc0a4506d1e7e362
Hitesh <hithesh2201@gmail.com>
2023-12-14 13:29:57 +0530

    doing automation

--- catalogue.sh
+++ catalogue.sh
@@ -29,12 +29,18 @@ CHECK "enabled"
 dnf install nodejs -y
 CHECK "nodejs"
 
-# Check if the user "roboshop" exists
-if id -u roboshop; then
-    echo "User roboshop exists."
+# User to add
+username="roboshop"
+
+# Check if the user exists
+if id "$username" >/dev/null 2>&1; then
+    echo "User $username already exists."
 else
-    useradd roboshop
+    # Add the user
+    useradd "$username"
+    echo "User $username added successfully."
 fi
+
 
 mkdir -p /app 
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
