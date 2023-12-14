cc95ee6727608909c22bae578dfd5dfb5600d072
Hitesh <hithesh2201@gmail.com>
2023-12-14 16:18:06 +0530

    doing automation

--- catalogue.sh
+++ catalogue.sh
@@ -24,20 +24,20 @@ else
 fi
 
 # Disable and enable Node.js module
-dnf module list --disabled nodejs &>/dev/null
+dnf module list --disabled nodejs &>>$LOGFILE
 if [ $? -eq 0 ]; then
     dnf module disable nodejs -y
     CHECK "disabled"
 fi
 
-dnf module list --enabled nodejs:18 &>/dev/null
+dnf module list --enabled nodejs:18 &>>$LOGFILE
 if [ $? -ne 0 ]; then
     dnf module enable nodejs:18 -y
     CHECK "enabled"
 fi
 
 # Install Node.js
-dnf list installed nodejs &>/dev/null
+dnf list installed nodejs &>>$LOGFILE
 if [ $? -ne 0 ]; then
     dnf install nodejs -y
     CHECK "nodejs"
@@ -47,7 +47,7 @@ fi
 username="roboshop"
 
 # Check if the user exists
-id "$username" &>/dev/null
+id "$username" &>>$LOGFILE
 if [ $? -eq 0 ]; then
     echo "User $username already exists."
 else
@@ -76,9 +76,9 @@ systemctl daemon-reload
 CHECK "Reloaded SystemD"
 
 # Enable and start the catalogue service
-systemctl enable catalogue
+systemctl enable catalogue &>>$LOGFILE
 CHECK "Enabled catalogue service"
-systemctl start catalogue
+systemctl start catalogue &>>$LOGFILE
 CHECK "Started catalogue service"
 
 # Copy MongoDB repository file
@@ -88,12 +88,12 @@ CHECK "Copied mongo.repo"
 # Install MongoDB shell
 dnf list installed mongodb-org-shell &>/dev/null
 if [ $? -ne 0 ]; then
-    dnf install mongodb-org-shell -y
+    dnf install mongodb-org-shell -y &>>$LOGFILE
     CHECK "Installed MongoDB shell"
 fi
 
 # Load MongoDB schema
-mongo --host mongo.hiteshshop.online </app/schema/catalogue.js
+mongo --host mongo.hiteshshop.online </app/schema/catalogue.js &>>$LOGFILE
 CHECK "Loaded MongoDB schema"
 
 echo -e "$G Script run successfully"
