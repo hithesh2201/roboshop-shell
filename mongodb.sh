b914ec037c0bae05e5e20541cc1f1e66c31a55e5
Hitesh <hithesh2201@gmail.com>
2023-12-14 10:29:25 +0530

    doing automation

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+NORMAL="\e[0m"
+
 if [ "$ID" -ne 0 ];
 then
-    echo "Use root user"
+    echo " $R Use root user"
+else
+    echo "$G ROOT USER"  
 fi
 
     
