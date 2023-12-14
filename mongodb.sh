65f666f3a5d0b8058cfc3c0beb780e33da80cb70
Hitesh <hithesh2201@gmail.com>
2023-12-14 10:24:51 +0530

    doing automation

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 ID=$(id -u)
-if [ "$ID" -eq 0 ];
+if [ "$ID" -ne 0 ];
 then
-    echo "Already Root user"
-else
-    $(sudo su -)
-    echo "made root user"
+    echo "Use root user"
 fi
 
     
